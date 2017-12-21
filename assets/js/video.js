15448e7dcf1c3b9819380702b635ae44c017652c
Rany Keddo <purzelrakete@gmail.com>
2017-12-21 20:33:13 +0100

    Open and close

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,51 +1,32 @@
 (function() {
 
-  // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
-  // e.g. just plyr.setup(); and leave it at that if you have no need for events
-  var instances = plyr.setup({
-    // Output to console
-    debug: true
-  });
+  // video
+  var instances = plyr.setup();
 
-  // Get an element
-  function get(selector) {
-    return document.querySelector(selector);
-  }
+  function get(selector) { return document.querySelector(selector); }
 
-  // Custom event handler (just for demo)
   function on(element, type, callback) {
     if (!(element instanceof HTMLElement)) {
       element = get(element);
     }
+
     element.addEventListener(type, callback, false);
   }
 
-
-  // Loop through each instance
-  instances.forEach(function(instance) {
-    // Play
-    on('.js-play', 'click', function() {
-      instance.play();
-    });
-
-    // Pause
-    on('.js-pause', 'click', function() {
-      instance.pause();
-    });
-
-    // Stop
-    on('.js-stop', 'click', function() {
-      instance.stop();
-    });
-
-    // Rewind
-    on('.js-rewind', 'click', function() {
-      instance.rewind();
-    });
-
-    // Forward
-    on('.js-forward', 'click', function() {
-      instance.forward();
-    });
+  on('.about-button', 'click', function() {
+    get('.about').className = 'about active';
   });
+
+  on('.close-about-button', 'click', function() {
+    get('.about').className = 'about';
+  });
+
+  instances.forEach(function(instance) {
+    on('.js-play', 'click', function() { instance.play(); });
+    on('.js-pause', 'click', function() { instance.pause(); });
+    on('.js-stop', 'click', function() { instance.stop(); });
+    on('.js-rewind', 'click', function() { instance.rewind(); });
+    on('.js-forward', 'click', function() { instance.forward(); });
+  });
+
 })();
